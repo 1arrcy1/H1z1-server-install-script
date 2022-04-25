17f0a51037c3b308bdca56a6e06e86467da5938a
1arrcy1 <69429134+1arrcy1@users.noreply.github.com>
2022-04-25 23:19:22 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,6 +18,7 @@ echo "installing GUI web"
 
 npm i pm2 -g
 npm i -g mongo-gui
+pm2 kill
 pm2 start mongo-gui
 npm cache clean -f
 npm install -g n
@@ -37,7 +38,6 @@ export CLIENT_SIXTEEN="true"
 export LOGINSERVER_IP="127.0.0.1"
 export MONGO_URL="mongodb://localhost:27017/"
 
-pm2 kill
 pm2 start out/zoneserver.js --watch
 export DEBUG="*"
 pm2 start out/loginserver.js --watch
