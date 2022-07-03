e20a43e6ed40aa2a9c8f15dcb4168e2d55a8bf2d
1arrcy1 <69429134+1arrcy1@users.noreply.github.com>
2022-07-03 02:47:13 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,35 +1,41 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+function Invoke-PowerShell {
+    powershell -nologo
+    Invoke-PowerShell
+}
 choco install -y git
-choco install -y nvm
+choco install -y --force nodejs
 choco install mongodb
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
-nvm install 16.13.1
-$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
-nvm use 16.13.1
+function Invoke-PowerShell {
+    powershell -nologo
+    Invoke-PowerShell
+}
+function Invoke-PowerShell {
+    powershell -nologo
+    Invoke-PowerShell
+}
+ 
  
 git clone https://github.com/H1emu/h1z1-server
 cd h1z1-server
-nvm install
+npm install
  
-NewItem ZoneServerArrcy.ps1
+New-Item ZoneServerArrcy.ps1
 Add-Content ZoneServerArrcy.ps1 '$env:DEBUG="ZoneServer"'
 Add-Content ZoneServerArrcy.ps1 '$env:CLIENT_SIXTEEN="true"'
-Add-Content ZoneServerArrcy.ps1 'echo LOGINSERVER if you want to use h1emu community list'
 Add-Content ZoneServerArrcy.ps1 '$env:WORLD_ID="2"'
 Add-Content ZoneServerArrcy.ps1 '$env:LOGINSERVER_IP="127.0.0.1"'
 Add-Content ZoneServerArrcy.ps1 '$env:MONGO_URL="mongodb://localhost:27017/"'
-Add-Content ZoneServerArrcy.ps1 'node out/servers/LoginServer/zoneserver.js'
+Add-Content ZoneServerArrcy.ps1 'node docker/2016/zoneServer.js'
  
 New-Item LoginServerArrcy.ps1
 Add-Content LoginServerArrcy.ps1 '$env:DEBUG="*"'
 Add-Content LoginServerArrcy.ps1 '$env:CLIENT_SIXTEEN="true"'
-Add-Content LoginServerArrcy.ps1 'echo LOGINSERVER if you want to use h1emu community list'
 Add-Content LoginServerArrcy.ps1 '$env:WORLD_ID="2"'
 Add-Content LoginServerArrcy.ps1 '$env:LOGINSERVER_IP="127.0.0.1"'
 Add-Content LoginServerArrcy.ps1 '$env:MONGO_URL="mongodb://localhost:27017/"'
-Add-Content LoginServerArrcy.ps1 'node out/servers/LoginServer/loginserver.js'
+Add-Content LoginServerArrcy.ps1 'node docker/2015/loginServer.js'
  
  
-start powershell {./LoginServerArrcy.ps1; exit}
+start powershell {./LoginServerArrcy.ps1; Read-Host}
 start powershell {./ZoneServerArrcy.ps1; Read-Host}
