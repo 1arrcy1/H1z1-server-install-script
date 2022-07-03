3aa6f738bdb01e99bc0cbfacee9f1a88e799f8a2
1arrcy1 <69429134+1arrcy1@users.noreply.github.com>
2022-07-03 02:59:25 +0200

    Add files via upload

---
+++ install-server.ps1
@@ -0,0 +1,23 @@
+git clone https://github.com/H1emu/h1z1-server
+cd h1z1-server
+npm install
+ 
+New-Item ZoneServerArrcy.ps1
+Add-Content ZoneServerArrcy.ps1 '$env:DEBUG="ZoneServer"'
+Add-Content ZoneServerArrcy.ps1 '$env:CLIENT_SIXTEEN="true"'
+Add-Content ZoneServerArrcy.ps1 '$env:WORLD_ID="2"'
+Add-Content ZoneServerArrcy.ps1 '$env:LOGINSERVER_IP="127.0.0.1"'
+Add-Content ZoneServerArrcy.ps1 '$env:MONGO_URL="mongodb://localhost:27017/"'
+Add-Content ZoneServerArrcy.ps1 'node docker/2016/zoneServer.js'
+ 
+New-Item LoginServerArrcy.ps1
+Add-Content LoginServerArrcy.ps1 '$env:DEBUG="*"'
+Add-Content LoginServerArrcy.ps1 '$env:CLIENT_SIXTEEN="true"'
+Add-Content LoginServerArrcy.ps1 '$env:WORLD_ID="2"'
+Add-Content LoginServerArrcy.ps1 '$env:LOGINSERVER_IP="127.0.0.1"'
+Add-Content LoginServerArrcy.ps1 '$env:MONGO_URL="mongodb://localhost:27017/"'
+Add-Content LoginServerArrcy.ps1 'node docker/2015/loginServer.js'
+ 
+ 
+start powershell {./LoginServerArrcy.ps1; Read-Host}
+start powershell {./ZoneServerArrcy.ps1; Read-Host}
