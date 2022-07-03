070f30393cf6dedb737f4f039d9b19b28bdcbb22
1arrcy1 <69429134+1arrcy1@users.noreply.github.com>
2022-07-03 02:12:38 +0200

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,36 +1,35 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-. $profile
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 choco install -y git
 choco install -y nvm
-choco install mongodb --pre 
-. $profile
- 
+choco install mongodb
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User") 
 nvm install 16.13.1
-. $profile
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
 nvm use 16.13.1
  
 git clone https://github.com/H1emu/h1z1-server
 cd h1z1-server
-npm install
-
+nvm install
+ 
 NewItem ZoneServerArrcy.ps1
-Set-Content ZoneServerArrcy.ps1 '$env:DEBUG="ZoneServer"'
-Set-Content ZoneServerArrcy.ps1 '$env:CLIENT_SIXTEEN="true"'
-Set-Content ZoneServerArrcy.ps1 'echo LOGINSERVER if you want to use h1emu community list'
-Set-Content ZoneServerArrcy.ps1 '$env:WORLD_ID="2"'
-Set-Content ZoneServerArrcy.ps1 '$env:LOGINSERVER_IP="127.0.0.1"'
-Set-Content ZoneServerArrcy.ps1 '$env:MONGO_URL="mongodb://localhost:27017/"'
-Set-Content ZoneServerArrcy.ps1 '$env:node out/servers/LoginServer/zoneserver.js'
+Add-Content ZoneServerArrcy.ps1 '$env:DEBUG="ZoneServer"'
+Add-Content ZoneServerArrcy.ps1 '$env:CLIENT_SIXTEEN="true"'
+Add-Content ZoneServerArrcy.ps1 'echo LOGINSERVER if you want to use h1emu community list'
+Add-Content ZoneServerArrcy.ps1 '$env:WORLD_ID="2"'
+Add-Content ZoneServerArrcy.ps1 '$env:LOGINSERVER_IP="127.0.0.1"'
+Add-Content ZoneServerArrcy.ps1 '$env:MONGO_URL="mongodb://localhost:27017/"'
+Add-Content ZoneServerArrcy.ps1 'node out/servers/LoginServer/zoneserver.js'
  
 New-Item LoginServerArrcy.ps1
-Set-Content LoginServerArrcy.ps1 '$env:DEBUG="*"'
-Set-Content LoginServerArrcy.ps1 '$env:CLIENT_SIXTEEN="true"'
-Set-Content LoginServerArrcy.ps1 'echo LOGINSERVER if you want to use h1emu community list'
-Set-Content LoginServerArrcy.ps1 '$env:WORLD_ID="2"'
-Set-Content LoginServerArrcy.ps1 '$env:LOGINSERVER_IP="127.0.0.1"'
-Set-Content LoginServerArrcy.ps1 '$env:MONGO_URL="mongodb://localhost:27017/"'
-Set-Content LoginServerArrcy.ps1 '$env:node out/servers/LoginServer/loginserver.js'
+Add-Content LoginServerArrcy.ps1 '$env:DEBUG="*"'
+Add-Content LoginServerArrcy.ps1 '$env:CLIENT_SIXTEEN="true"'
+Add-Content LoginServerArrcy.ps1 'echo LOGINSERVER if you want to use h1emu community list'
+Add-Content LoginServerArrcy.ps1 '$env:WORLD_ID="2"'
+Add-Content LoginServerArrcy.ps1 '$env:LOGINSERVER_IP="127.0.0.1"'
+Add-Content LoginServerArrcy.ps1 '$env:MONGO_URL="mongodb://localhost:27017/"'
+Add-Content LoginServerArrcy.ps1 'node out/servers/LoginServer/loginserver.js'
  
  
-start powershell {./LoginServerArrcy.ps1; Read-Host}
+start powershell {./LoginServerArrcy.ps1; exit}
 start powershell {./ZoneServerArrcy.ps1; Read-Host}
