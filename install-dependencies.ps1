4dcf95676abf9bcc5e2b97341da20caf708876d3
1arrcy1 <69429134+1arrcy1@users.noreply.github.com>
2022-07-03 03:54:32 +0200

    Update install-dependencies.ps1

--- install-dependencies.ps1
+++ install-dependencies.ps1
@@ -1,4 +1,4 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y git
 choco install -y --force nodejs
-choco install mongodb
+choco install mongodb.install --version=4.0.7
